e432d377d6d063247fbde7559a107ac9eab098e0
Niklas Eicker <git@nikl.me>
2024-04-18 18:44:14 +0200

    Show page counter on page 11

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,3 +1,3 @@
 <template>
-  <footer class="absolute right-0 bottom-0 p-2" v-if="$slidev.nav.currentPage !== 1 && $slidev.nav.currentPage !== 11"><SlideCurrentNo />/<SlidesTotal /></footer>
+  <footer class="absolute right-0 bottom-0 p-2" v-if="$slidev.nav.currentPage !== 1"><SlideCurrentNo />/<SlidesTotal /></footer>
 </template>
